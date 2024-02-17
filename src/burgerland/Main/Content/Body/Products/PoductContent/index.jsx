ad191f49288e646869005fe570240847e975f95f
Ali_Ostad <ali.ostadhosein@gmail.com>
2024-02-17 18:34:15 +0330

    merge commit sixth

---
+++ src/burgerland/Main/Content/Body/Products/PoductContent/index.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { IoMdEye, IoIosSettings } from "react-icons/io";
+import Card from "../Card";
+import NewProductPage from "../../../../SideData/NewProductPage";
+
+const ProductContent = () => {
+  const [showAddProductPage, setShowAddProductPage] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const handleShowAddProductPage = () => {
+    setShowAddProductPage(true);
+  };
+
+  const handleCloseAddProductPage = (newProduct) => {
+    setShowAddProductPage(false);
+    if (newProduct) {
+      setProducts((prevProducts) => [...prevProducts, newProduct]);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    setShowAddProductPage(false);
+  };
+
+  return (
+    <div className="flex flex-wrap  max-h-[593px] mt-8 mb-3 gap-5 overflow-y-auto">
+      <Card />
+
+      {products.map((product, index) => (
+        <>
+          <div className="flex bg-white flex-col justify-start shadow-md rounded-2xl  max-w-[190px] h-[280px] overflow-hidden mb-2">
+            <div className="products-container">
+              <div key={index} className="product-card">
+                <div className="flex flex-col items-center justify-center p-6">
+                  <h1 className="capitalize font-bold">{product.name}</h1>
+                  <span className="font-thin">{product.weight}</span>
+                  <p className="text-xs font-thin text-center">
+                    Folan folan, Felan felan, Filan filan
+                  </p>
+                </div>
+                <div className="flex items-center justify-between px-6 gap-5">
+                  <div className="flex justify-center items-center">
+                    <span className="text-[20px]">${product.price}</span>
+                  </div>
+                  <div className="flex justify-center items-center border rounded">
+                    <span className="p-1 cursor-pointer">
+                      <IoMdEye size={20} />
+                    </span>
+                  </div>
+                  <div className="flex justify-center items-center border rounded">
+                    <span className="p-1 cursor-pointer">
+                      <IoIosSettings size={20} />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ))}
+
+      <div
+        className="mb-2 flex justify-center items-center bg-white flex-col cursor-pointer shadow-md rounded-2xl  w-[190px] h-[280px] overflow-hidden text-lg font-bold"
+        onClick={handleShowAddProductPage}
+      >
+        <pre>+</pre>
+        <pre>Add New</pre> <pre>Product</pre>
+      </div>
+      {showAddProductPage && (
+        <NewProductPage
+          onClose={handleCloseAddProductPage}
+          onSubmit={handleSubmit}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ProductContent;
--- src/burgerland/Main/Content/Body/Products/index.jsx
+++ src/burgerland/Main/Content/Body/Products/index.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
 
-
-import ProductInfo from "./ProductInfo";
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductNavbar from "./ProductNavbar";
+import ProductContent from "./PoductContent";
 
 
 const Products = () => {
 
-
   return (
     <section className=" w-full h-full flex flex-col ">
-      <ProductNavbar/>
-      
-      {for(i=0;i<Array.length;i++){
-        <ProductInfo />
-      }}
+      <BrowserRoute>
+        <ProductNavbar />
+        <ProductContent>
+        <Routes>
+          <Route path="" element={}></Route>
+        </Routes>
+        </ProductContent>
+      </BrowserRoute>
+
     </section>
   );
 };
