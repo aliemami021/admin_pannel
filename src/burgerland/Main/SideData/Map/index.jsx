4461ccf6a9b004da8f664913215c0eac732bc0be
Ali_Ostad <ali.ostadhosein@gmail.com>
2024-02-17 18:07:07 +0330

    restaurant card

--- src/burgerland/Main/SideData/Map/index.jsx
+++ src/burgerland/Main/SideData/Map/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Map = () => {
   return (
-    <div className='bg-red-300 absolute  top-0 right-0 w-[40%] h-full z-50 flex justify-center items-center p-7'>Map</div>
+    <div className='bg-white absolute  top-0 right-0 w-[40%] h-full z-50 flex justify-center items-center p-7 shadow-inner'>Map</div>
   )
 }
 
